6363e3a19650aebe758edcb8d67dbefa80e608f4
Pratik Singh <pratikkumars623@gmail.com>
2024-09-03 23:55:09 +0530

    Little changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,redirect,render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,10 @@ def index():
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
+
+@app.route('/logout')
+def logout():
+    return redirect("/")
 
 if __name__=="__main__":
     app.run(debug=True,port=5000)
