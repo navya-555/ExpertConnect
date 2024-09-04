da9eedfccb4d251719a47877a11e14f9f6d4a986
Pratik Singh <pratikkumars623@gmail.com>
2024-09-04 12:21:13 +0530

    minor chnages

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ db=SQLAlchemy(app)
 class Expert(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    fullName = db.Column(db.String(120), nullable=False)
+    name= db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(15), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     file1= db.Column(db.LargeBinary)
@@ -21,21 +21,23 @@ class Expert(db.Model):
     link2 = db.Column(db.String(255), nullable=True)
 
 
-
 @app.route('/add_expert', methods=['GET', 'POST'])
 def expertAdd_page():
     if request.method == 'POST':
-        full_name = request.form['Fullname']
+        full_name = request.form['full_name']
         username= request.form['username']
         email = request.form['email']
         phone = request.form['phone']
         img_data = request.files['photo'].read() if 'photo' in request.files else None
         link1 = request.form['googleScholarLink']
         link2 = request.form['githubLink']
-        new_expert = Expert(full_name=full_name, username=username,email=email,phone=phone,img_data=img_data,link1=link1,link2=link2) 
+        new_expert = Expert(name=full_name, username=username,email=email,phone=phone,file1=img_data,link1=link1,link2=link2) 
         db.session.add(new_expert)
         db.session.commit()
         print("Added")
+    allRecords=Expert.query.all()
+    return render_template('index.html',allRecords=allRecords)
+
 
 
 
