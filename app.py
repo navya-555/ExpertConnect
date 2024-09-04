86bbfdb3f04fe628f83f0448d29be55b6f651120
Sufyan <bucks.pri@gmail.com>
2024-09-04 16:12:29 +0530

    Db removed

--- app.py
+++ app.py
@@ -3,43 +3,6 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///expert.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-db=SQLAlchemy(app)
-
-
-class Expert(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    name= db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(15), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    file1= db.Column(db.LargeBinary)
-    file2= db.Column(db.LargeBinary)
-    file3= db.Column(db.LargeBinary)
-    link1 = db.Column(db.String(255), nullable=True)
-    link2 = db.Column(db.String(255), nullable=True)
-
-
-@app.route('/add_expert', methods=['GET', 'POST'])
-def expertAdd_page():
-    if request.method == 'POST':
-        full_name = request.form['full_name']
-        username= request.form['username']
-        email = request.form['email']
-        phone = request.form['phone']
-        img_data = request.files['photo'].read() if 'photo' in request.files else None
-        link1 = request.form['googleScholarLink']
-        link2 = request.form['githubLink']
-        new_expert = Expert(name=full_name, username=username,email=email,phone=phone,file1=img_data,link1=link1,link2=link2) 
-        db.session.add(new_expert)
-        db.session.commit()
-        print("Added")
-    allRecords=Expert.query.all()
-    return render_template('index.html',allRecords=allRecords)
-
-
-
 
 
 @app.route('/')
@@ -54,6 +17,10 @@ def dashboard():
 @app.route('/logout')
 def logout():
     return redirect("/")
+
+@app.route('/candidate_form')
+def candidate_form():
+    return render_template("candidate_form.html")
 
 if __name__=="__main__":
     app.run(debug=True,port=5000)
