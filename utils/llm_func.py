9e45159061264a908f87f77207c9a89d60e33e2a
Sufyan <bucks.pri@gmail.com>
2024-09-05 19:25:22 +0530

    Jd Portion

--- utils/llm_func.py
+++ utils/llm_func.py
@@ -46,6 +46,28 @@ def skill_domain_from_txt(text,model=gemini_model):
     output = response.text.strip().split('\n')
     return output
 
+def skill_domain_from_job_des(text,model=gemini_model):
+    prompt = f"""
+    You are analyzing a job. Identify the key skills and domains based on the description. Do not include any other information. Also make sure you only highlight prominent skills.
+
+    Output only the keywords for:
+    Skills: (e.g., Object Detection, OpenCV, Python)
+    Domains: (e.g., Computer Vision, Machine Learning)
+
+    You are supposed to follow these instructions while giving the output strictly.
+    1. Don't write anything else apart from skills and domain.
+    2. Use only ',' for separating two skills or domains.
+    3. Strictly follow the output format of the sample output.
+
+    Sample output:
+    Object Detection, OpenCV, Python
+    Computer Vision, Machine Learning
+    """
+    
+    response = model.generate_content([prompt, text])
+    output = response.text.strip().split('\n')
+    return output
+
 def similarity_score(tup1,tup2):
     skill_similarity = cosine_similarity([tup1[0]], [tup2[0]])
     domain_similarity = cosine_similarity([tup1[1]], [tup2[1]])
@@ -78,32 +100,6 @@ def process_gscholar(url):
     domain=gscholar_scraper(url)
     emb=generate_embeddings(domain)
     return emb
-
-
-
-def skill_domain_from_job_des(text,model=gemini_model):
-    prompt = f"""
-    You are analyzing a job. Identify the key skills and domains based on the description. Do not include any other information. Also make sure you only highlight prominent skills.
-
-    Output only the keywords for:
-    Skills: (e.g., Object Detection, OpenCV, Python)
-    Domains: (e.g., Computer Vision, Machine Learning)
-
-    You are supposed to follow these instructions while giving the output strictly.
-    1. Don't write anything else apart from skills and domain.
-    2. Use only ',' for separating two skills or domains.
-    3. Strictly follow the output format of the sample output.
-
-    Sample output:
-    Object Detection, OpenCV, Python
-    Computer Vision, Machine Learning
-    """
-    
-    response = model.generate_content([prompt, text])
-    output = response.text.strip().split('\n')
-    return output
-
-
 
 def process_job_des(pdf_data):
     text=extract_text_from_pdf(pdf_data)
